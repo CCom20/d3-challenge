591a0ad7ecdb1380665a7ca3a557df73d1bc9719
Cole Comstock <cole.d.comstock@gmail.com>
2021-01-14 19:58:10 -0700

    working on bubble chart

--- js/app.js
+++ js/app.js
@@ -1,12 +1,58 @@
-d3.csv("/data/data.csv").then(function(data){
+let bblSVGwidth = 750; 
+let bblSVGheight = 500; 
 
-    stateNames = data.map(item => item.state);
-    // console.log(stateNames)
+let svg = d3.select("#scatter")
+    .append("svg")
+    .attr("width", bblSVGwidth)
+    .attr("height", bblSVGheight)
 
-    d3.select("#scatter").append("ul").selectAll("li")
-        .data(stateNames)
+let svgMargins = {
+    top: 25,
+    right: 25,
+    bottom: 25,
+    left: 75
+}
+
+var chartHeight = bblSVGheight - svgMargins.top - svgMargins.bottom;
+var chartWidth = bblSVGwidth - svgMargins.left - svgMargins.right;
+
+let chartGroup = svg.append("g")
+    .attr("transform", `translate(${svgMargins.left}, ${svgMargins.top}`)
+
+
+d3.csv("data/data.csv").then(function(data){
+
+    data.forEach(function(data){
+        data.poverty = +data.poverty;
+        data.healthcare = +data.healthcare;
+    })
+
+    let yScale = d3.scaleLinear()
+        .domain([d3.min(data, d => data.healthcare) * .5,
+             d3.max(data, d => data.healthcare) * 1.5])
+        .range([chartHeight, 0]);
+    
+    let xScale = d3.scaleLinear()
+        .domain([d3.min(data , d => data.poverty) * .5, 
+            d3.max(data, d => data.poverty)  * 1.5])
+        .range([0, chartWidth])
+    
+    let yAxis = d3.axisLeft(yScale)
+    let xAxis = d3.axisBottom(xScale)
+
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${chartHeight})`)
+        .call(xAxis);
+    
+    chartGroup.append("g")
+        .call(yAxis)
+
+    chartGroup.selectAll(".bar")
+        .data(data)
         .enter()
-        .append("li")
-        .text(function(d) { 
-            return d});
+        .append("circle")
+        .attr("cx", d => xScale(data.poverty))
+        .attr("cy", d => yScale(data.healthcare))
+        .attr("height", d => chartHeight - yScale(d));
+      
 })
