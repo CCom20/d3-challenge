900e2c19d940ad44167fa4fc41ef6282982084dc
Cole Comstock <cole.d.comstock@gmail.com>
2021-01-15 19:12:22 -0700

    Working on tool tips, made it responsive

--- js/app.js
+++ js/app.js
@@ -1,64 +1,102 @@
-let bblSVGwidth = 850; 
-let bblSVGheight = 500; 
+function respBubbleChart() {
+    d3.csv("../data/data.csv").then(function(data){
 
-let svg = d3.select("#scatter")
-    .append("svg")
-    .attr("width", bblSVGwidth)
-    .attr("height", bblSVGheight)
+        let svgBubble = d3.select("#scatter").select("svg");
 
-let svgMargins = {
-    top: 25,
-    right: 25,
-    bottom: 25,
-    left: 25
+        if (!svgBubble.empty()) {
+            svgBubble.remove();
+            }
+
+        let bubbleWidth = +d3.select("#scatter").style("width").slice(0, -2);
+        let bubbleHeight = 500;
+    
+        let svg = d3.select("#scatter")
+            .append("svg")
+            .attr("width", bubbleWidth)
+            .attr("height", bubbleHeight); 
+    
+        let divWidth = +d3.select("#scatter").style("width").slice(0, -2);
+            console.log(divWidth); 
+    
+        let svgMargins = {
+            top: 25,
+            right: 50,
+            bottom: 50,
+            left: 50
+        }; 
+    
+        var chartHeight = bubbleHeight - svgMargins.top - svgMargins.bottom;
+        var chartWidth = bubbleWidth - svgMargins.left - svgMargins.right;
+    
+        let chartGroup = svg.append("g")
+        .attr("transform", `translate(${svgMargins.left / 2}, ${svgMargins.top / 2})`); 
+    
+        data.forEach(function(data){
+            data.poverty = +data.poverty;
+            data.healthcare = +data.healthcare;
+        }); 
+    
+        poverty = data.map(item => item.poverty); 
+        healthcare = data.map(item => item.healthcare); 
+    
+        let yScale = d3.scaleLinear()
+            .domain([0, d3.max(data, data => data.healthcare)])
+            .range([chartHeight, 0]);
+        
+        let xScale = d3.scaleLinear()
+            .domain([d3.min(data , data => data.poverty) * .7, 
+                d3.max(data, data => data.poverty)])
+            .range([0, chartWidth]); 
+        
+        let yAxis = d3.axisLeft(yScale); 
+        let xAxis = d3.axisBottom(xScale); 
+    
+    
+        chartGroup.append("g")
+            .attr("transform", `translate(${svgMargins.left}, ${chartHeight})`)
+            .call(xAxis);
+        
+        chartGroup.append("g")
+            .attr("transform", `translate(${svgMargins.bottom}, 0)`)
+            .call(yAxis); 
+        
+        chartGroup.append("text")
+            .attr("transform", `translate(${bubbleWidth / 2}, ${chartHeight + svgMargins.top * 2})`)
+            .attr("text-anchor", "middle")
+            .attr("font-size", "20px")
+            .attr("fill", "black")
+            .text("% in Poverty");
+        
+        chartGroup.append("text")
+            .attr("transform", `translate(0, ${chartHeight / 2}) rotate(-90)`)
+            .attr("text-anchor", "middle")
+            .attr("font-size", "20px")
+            .attr("fill", "black")
+            .text("% Lacking Healthcare");
+        
+        chartGroup.append("g").selectAll("text")
+            .data(data)
+            .enter()
+            .append("text")
+            .text(d => d.abbr)
+            .attr("x", d => xScale(d.poverty))
+            .attr("y", d => yScale(d.healthcare) + 5)
+            .attr("fill", "black")
+            .attr("text-anchor", "middle"); 
+    
+        chartGroup.append("g").selectAll("circle")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr("cx", (d, i) => xScale(poverty[i]))
+            .attr("cy", (d, i) => yScale(healthcare[i]))
+            .attr("height", d => chartHeight - yScale(d))
+            .attr("r", 15)
+            .attr("fill", "steelblue")
+            .attr("opacity", ".5");
+    });
 }
 
-var chartHeight = bblSVGheight - svgMargins.top - svgMargins.bottom;
-var chartWidth = bblSVGwidth - svgMargins.left - svgMargins.right;
+respBubbleChart();
 
-let chartGroup = svg.append("g")
-    .attr("transform", `translate(${svgMargins.left}, ${svgMargins.top}`)
-
-
-d3.csv("data/data.csv").then(function(data){
-
-    data.forEach(function(data){
-        data.poverty = +data.poverty;
-        data.healthcare = +data.healthcare;
-    }); 
-
-    poverty = data.map(item => item.poverty); 
-    healthcare = data.map(item => item.healthcare); 
-
-    let yScale = d3.scaleLinear()
-        .domain([0, d3.max(data, data => data.healthcare)])
-        .range([chartHeight, 0]);
-    
-    let xScale = d3.scaleLinear()
-        .domain([d3.min(data , data => data.poverty) * .5, 
-            d3.max(data, data => data.poverty)  * 1.5])
-        .range([0, chartWidth])
-    
-    let yAxis = d3.axisLeft(yScale)
-    let xAxis = d3.axisBottom(xScale)
-
-    chartGroup.append("g")
-        .attr("transform", `translate(${svgMargins.left}, ${chartHeight})`)
-        .call(xAxis);
-    
-    chartGroup.append("g")
-        .attr("transform", `translate(${svgMargins.bottom}, 0)`)
-        .call(yAxis)
-
-    let chartCircles = chartGroup.selectAll("circle")
-        .data(data)
-        .enter()
-        .append("circle")
-        .attr("cx", (d, i) => xScale(poverty[i]))
-        .attr("cy", (d, i) => yScale(healthcare[i]))
-        .attr("height", d => chartHeight - yScale(d))
-        .attr("r", 20)
-        .attr("fill", "lightblue")
-        .attr("opacity", ".5");
-      
-})
+d3.select(window).on("resize", respBubbleChart);
